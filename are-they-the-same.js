d3360d4871a45b5f4f95d166cccb1a4536f4abd7
Kristopher boyd <krisboyd1@gmail.com>
2022-05-02 11:06:44 -0700

    Add are-they-the-same.js

---
+++ are-they-the-same.js
@@ -0,0 +1,11 @@
+function comp(array1, array2){
+    if (!array1 || !array2) return false;
+    
+    array1.sort((a,b) => a-b);
+    array2.sort((a,b) => a-b);
+    
+    return array1.every((element, i) => {
+      return element**2 == array2[i]
+    })
+    
+  }
