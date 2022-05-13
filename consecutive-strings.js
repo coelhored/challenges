11b394e60a726f32ad26dbc688df0092dce9d70b
Kristopher boyd <krisboyd1@gmail.com>
2022-05-13 14:57:13 -0700

    Add sentence-smash.js

---
+++ consecutive-strings.js
@@ -0,0 +1,44 @@
+// Concatenate the consecutive strings of strarr by 2, we get:
+
+// treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+// folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+// trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+// blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+// abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+// Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+// The first that came is "folingtrashy" so 
+// longest_consec(strarr, 2) should return "folingtrashy".
+
+// In the same way:
+// longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+
+
+// my solution
+function longestConsec(strarr, k) {
+    let longest = "";
+ if (k>strarr.length || k <=0) return ""
+   for (i=0; i < strarr.length; i++){
+     concatenated = ""
+     for (j=0; j < k; j++){
+       concatenated += strarr[i+j] || ""
+     }
+     if (concatenated.length > longest.length) {
+       longest = concatenated;
+     }
+   }
+   return longest 
+ }
+
+//  better solution
+function longestConsec(strarr, k) {
+    var longest = "";
+    for(var i=0;k>0 && i<=strarr.length-k;i++){
+      var tempArray = strarr.slice(i,i+k);
+      var tempStr = tempArray.join("");
+      if(tempStr.length > longest.length){
+        longest = tempStr;
+      }
+    }
+    return longest;
+}
