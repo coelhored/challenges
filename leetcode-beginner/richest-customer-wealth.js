6b076a000e16ab3b148b3fc8f5869a3bafeb3c80
Kristopher boyd <krisboyd1@gmail.com>
2023-02-20 10:50:09 -0800

    add richest-customer-wealth.js

---
+++ leetcode-beginner/richest-customer-wealth.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+    /*    let biggestWealth = 0
+       let wealth
+       for (let i = 0; i < accounts.length; i++) {
+           wealth = 0
+           for (let j = 0; j < accounts[i].length; j++) {
+               wealth += accounts[i][j]
+           }
+           if (wealth > biggestWealth) {biggestWealth = wealth}
+       } */
+   
+       let sums  = accounts.map (x => {
+           return x.reduce((a,b) => a+b)
+       })
+       return Math.max(...sums)
+   };
