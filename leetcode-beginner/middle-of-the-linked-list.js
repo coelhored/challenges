9bebf3e5b538d0c0bbb953dfb411eccc8ca33687
Kristopher boyd <krisboyd1@gmail.com>
2023-02-27 13:36:57 -0800

    add middle-of-the-linked-list.js

---
+++ leetcode-beginner/middle-of-the-linked-list.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let end = head
+    let middle = head
+    while (end && end.next) {
+        end = end.next.next
+        middle = middle.next
+    }
+    return middle
+};
